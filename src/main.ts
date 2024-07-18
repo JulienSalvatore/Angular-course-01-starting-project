04b3a018305dd2de7675eff0315f8d7b65d76bc7
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-18 22:05:01 +0200

    change data dynamicly with user name and user picture

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HeaderComponent } from './app/header.component';
+import { HeaderComponent } from './app/header/header.component';
+import { UserComponent } from './app/user/user.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
-
