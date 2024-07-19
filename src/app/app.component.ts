6683e36c7e960bc62a5a3e846a462b892cf31cdc
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-19 19:28:39 +0200

    dynamicly tasks changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { TasksComponent } from './tasks/tasks.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
