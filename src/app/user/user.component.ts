eb6d17336833c11f2d43e2499531f1721774bb21
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-19 18:07:11 +0200

    increase readibility by changing to simplify code and use objects data storage

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,16 +15,18 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
