076f856c5ee640bb8c203cefeae45e1b54a5c7af
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-20 14:17:06 +0200

    use ng-content to have custom wrapper between html files

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,12 +9,14 @@ import {
 } from '@angular/core';
 
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
