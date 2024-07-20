c68a824e2943d1c353d74d08e94220a5ec7dcbf1
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-20 13:21:28 +0200

    two way binding for input field

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle = '';
 
   onCancel() {
     this.cancel.emit();
