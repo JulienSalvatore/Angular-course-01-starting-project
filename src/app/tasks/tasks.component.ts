2d321551464f3fd687bb8926ab58591beb170638
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-20 12:45:42 +0200

    Remove users tasks when it's complete

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -40,4 +40,8 @@ export class TasksComponent {
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 }
